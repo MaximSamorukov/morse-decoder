fc37a24682cf87085e7a4a347e17093f4d203cd8
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-09-11 15:15:09 +0300

    init: create app

--- src/index.js
+++ src/index.js
@@ -35,10 +35,50 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' '
 };
+const table = {
+    '10': '.',
+    '11': '-',
+    '00': '',
+    '**': '**'
+}
 
 function decode(expr) {
     // write your solution here
+    let strArray = [];
+    const recursiveFunc = (str) => {
+        const str1 = str.slice(0, 10);
+        strArray.push(str1);
+        if (str.length >= 10) {
+            const newString = str.slice(10);
+            recursiveFunc(newString);
+        }
+    }
+    recursiveFunc(expr);
+
+    const recursiveFunc2 = (str, ar = []) => {
+        const item = str.slice(0, 2);
+
+        if (item !== '') {
+            ar.push(item);
+        }
+        if (str.length > 2) {
+            const newStr2 = str.slice(2);
+            recursiveFunc2(newStr2, ar);
+        }
+    return ar
+    }
+    const finalArray = strArray.map((i) => {
+        const element = recursiveFunc2(i);
+        return element;
+    });
+    const letterArray = finalArray
+    .map((i) => i.map((ii) => table[ii])
+    .join(''))
+    .map((i) => MORSE_TABLE[i]).join('');
+    return letterArray;
+
 }
 
 module.exports = {
